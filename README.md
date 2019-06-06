6de9e5e8f1d72f89ae37b607cb467b9c1899b61f
SinghDigamber <digambersingh126@gmail.com>
2019-06-06 18:18:53 +0530

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Angular8SelectDropdown
+Angular 8 Select Dropdown Tutorial with Reactive Forms - We will learn to implement, create and validate Select dropdown with Reactive Forms.
+
+## Tutorial
+[Angular 8 Select Dropdown Tutorial with Reactive Forms](https://www.positronx.io/angular-8-select-dropdown-tutorial-with-reactive-forms/)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.1.
 
@@ -25,3 +29,4 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+# angular8-select-dropdown
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular8-select-dropdown';
+  submitted = false;
+  oppoSuits: any = ['Men', 'Women', 'Boys', 'Inspiration']
+
+  constructor(public fb: FormBuilder) { }
+
+  oppoSuitsForm = this.fb.group({
+    name: ['', [Validators.required]]
+  })
+
+  changeSuit(e) {
+    this.oppoSuitsForm.get('name').setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+
+  /* Select Dropdown error handling */
+  public handleError = (controlName: string, errorName: string) => {
+    return this.oppoSuitsForm.controls[controlName].hasError(errorName);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    alert(JSON.stringify(this.oppoSuitsForm.value))
+  }
 }
